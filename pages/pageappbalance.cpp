4f76e23591bccf4df066520e99af5f3e274e3fc5
Mitch Lustig <moesk8er2@gmail.com>
2019-09-10 19:30:09 -0700

    Add basic low voltage tiltback

--- pages/pageappbalance.cpp
+++ pages/pageappbalance.cpp
@@ -107,6 +107,7 @@ void PageAppBalance::setVesc(VescInterface *vesc)
         ui->configPane->addParamRow(mVesc->appConfig(), "app_balance_conf.tiltback_duty");
         ui->configPane->addParamRow(mVesc->appConfig(), "app_balance_conf.tiltback_angle");
         ui->configPane->addParamRow(mVesc->appConfig(), "app_balance_conf.tiltback_speed");
+        ui->configPane->addParamRow(mVesc->appConfig(), "app_balance_conf.tiltback_voltage");
         ui->configPane->addRowSeparator(tr("Overspeed"));
         ui->configPane->addParamRow(mVesc->appConfig(), "app_balance_conf.overspeed_duty");
         ui->configPane->addRowSeparator(tr("Fault"));

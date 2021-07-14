ce516800706e38e6c924cccd5ce2e6f860715939
Benjamin Vedder <benjamin@vedder.se>
2021-07-14 20:56:15 +0200

    Added power switch firmware

--- pages/pageswdprog.cpp
+++ pages/pageswdprog.cpp
@@ -534,6 +534,9 @@ void PageSwdProg::bmConnRes(int res)
         addSwdFw("Trampa 18s Light BMS",
                  "://res/firmwares_bms/18s_light/vesc_default.bin", 0,
                  "://res/bootloaders_bms/generic.bin", 0x3E000);
+        addSwdFw("Power Switch 120V",
+                 "://res/other_fw/vesc_power_switch_120.bin", 0,
+                 "://res/bootloaders_bms/generic.bin", 0x3E000);
         break;
 
     default:

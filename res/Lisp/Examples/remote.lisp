85028d299951182aac8aa9de001e1f9ec6433c2c
Benjamin Vedder <benjamin@vedder.se>
2022-03-13 10:16:40 +0100

    Added lisp remote example

---
+++ res/Lisp/Examples/remote.lisp
@@ -0,0 +1,14 @@
+(defun main () 
+    (progn
+        (define state (get-remote-state))
+        (define mote-y (ix state 0))
+        (define mote-x (ix state 1))
+        (define mote-c (ix state 2))
+        (define mote-z (ix state 3))
+        (define mote-rev (ix state 4))
+        
+        (sleep 0.02)
+        (main)
+))
+
+(main)

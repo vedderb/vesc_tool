f980e678830ebbf9e4ae6d7ec28fa7232b2528de
Benjamin Vedder <benjamin@vedder.se>
2022-03-28 23:34:39 +0200

    Updated lisp examples and syntax hl

--- res/Lisp/Examples/list_with_lists.lisp
+++ res/Lisp/Examples/list_with_lists.lisp
@@ -1,34 +1,38 @@
-(define name-default "VESC_default.bin")
-(define name-nolim "VESC_no_limits.bin")
+(def name-default "VESC_default.bin")
+(def name-nolim "VESC_no_limits.bin")
 
-(define pkg `(
+(def pkg `(
             ("46_o_47"
-                ("46" . ,name-default)
-                ("46_no_limits" . ,name-nolim)
-                ("46_33k" . "VESC_33k.bin")
-                ("46_0005ohm" . "VESC_0005ohm.bin")
+                ("46" ,name-default)
+                ("46_no_limits" ,name-nolim)
+                ("46_33k" "VESC_33k.bin")
+                ("46_0005ohm" "VESC_0005ohm.bin")
             )
             ("60"
-                ("60" . ,name-default)
-                ("60_no_limits" . ,name-nolim)
+                ("60" ,name-default)
+                ("60_no_limits" ,name-nolim)
             )
 ))
 
-; Print using loopforeach
-(loopforeach hw pkg
-    (progn
-        (print (str-merge "Dir: " (car hw)))
-        (loopforeach fw (cdr hw)
-            (print (str-merge "  Target: " (car fw) " File: " (cdr fw))))
-        (print " ")
+; Pad string s to length n with character ch, functional style
+(defun pad-str (s n ch)
+    (if (>= (str-len s) n)
+        s
+        (pad-str2 (str-merge s ch) n ch)
 ))
 
-; Same as above, but more functional approach for printing
-(map (lambda (index)
-    (let ( (dir (car (ix pkg index)))
-           (fws (cdr (ix pkg index))) )
-        (progn
-            (print (str-merge "Dir: " dir))
-            (map (lambda (x) (print (str-merge "  Target: " (car x) " File: " (cdr x)))) fws)
-            (print " ")
-))) (iota (length pkg)))
+; Same as above, imperative style
+(defun pad-str2 (s n ch)
+    (progn
+        (loopwhile (< (str-len s) n)
+            (set! 's (str-merge s ch)))
+        s
+))
+
+(loopforeach hw pkg
+    (progn
+        (print (str-merge "Dir: " (first hw)))
+        (loopforeach fw (rest hw)
+            (print (str-merge "  Target: " (pad-str (first fw) 12 " ") " File: " (second fw))))
+        (print " ")
+))

1dba19f024344daaa1efb8284e5fab0e6f462448
Benjamin Vedder <benjamin@vedder.se>
2022-11-18 09:56:15 +0100

    Use amplitude instead of gain setting for sin/cos encoder

--- res/Lisp/Examples/encoder_sincos_cal.lisp
+++ res/Lisp/Examples/encoder_sincos_cal.lisp
@@ -2,8 +2,8 @@
 
 ; How to use
 ; 1. Spin the motor slowly until s-min, s-max, c-min and c-max don't change any more
-; 2. The gains and offsets are now in s-gain, s-ofs, c-gain and c-ofs
-; 3. Go to FOC->Encoder and enter the calculated gains and offsets
+; 2. The amplitudes and offsets are now in s-amp, s-ofs, c-amp and c-ofs
+; 3. Go to FOC->Encoder and enter the calculated amplitudes and offsets
 
 (def raw-sin (get-adc 0))
 (def raw-cos (get-adc 1))
@@ -12,8 +12,8 @@
 (def out-cos 0.0)
 (def out-mod 0.0)
 
-(def s-gain 1.0)
-(def c-gain 1.0)
+(def s-amp 1.0)
+(def c-amp 1.0)
 
 (def s-ofs 1.65)
 (def c-ofs 1.65)
@@ -28,16 +28,16 @@
 
 (reset-max-min)
 
-(defun calc-gain-ofs (print-res)
+(defun calc-amp-ofs (print-res)
     (progn
         (def s-ofs (/ (+ s-max s-min) 2.0))
         (def c-ofs (/ (+ c-max c-min) 2.0))
-        (def s-gain (* 2.0 (/ 1.0 (- s-max s-min))))
-        (def c-gain (* 2.0 (/ 1.0 (- c-max c-min))))
+        (def s-amp (/ (- s-max s-min) 2.0))
+        (def c-amp (/ (- c-max c-min) 2.0))
         (if print-res
             (progn
-                (print (str-merge (str-from-n s-gain "Sin Gain: %.2f ") (str-from-n s-ofs "Sin Offset: %.2f ")))
-                (print (str-merge (str-from-n c-gain "Cos Gain: %.2f ") (str-from-n c-ofs "Cos Offset: %.2f ")))
+                (print (str-merge (str-from-n s-amp "Sin Amp: %.3f ") (str-from-n s-ofs "Sin Offset: %.3f ")))
+                (print (str-merge (str-from-n c-amp "Cos Amp: %.3f ") (str-from-n c-ofs "Cos Offset: %.3f ")))
 ))))
 
 (def filter-const 0.1)
@@ -55,12 +55,12 @@
         (if (> raw-cos c-max) (def c-max raw-cos))
         (if (< raw-cos c-min) (def c-min raw-cos))
         
-        (def out-sin (* (- raw-sin s-ofs) s-gain))
-        (def out-cos (* (- raw-cos c-ofs) c-gain))
+        (def out-sin (/ (- raw-sin s-ofs) s-amp))
+        (def out-cos (/ (- raw-cos c-ofs) c-amp))
         (def out-mod (sqrt (+ (* out-sin out-sin) (* out-cos out-cos))))
         
         (if (and (> (- s-max s-min) 0.1) (> (- c-max c-min) 0.1))
-            (calc-gain-ofs false)
+            (calc-amp-ofs false)
         )
 
         (sleep 0.01)

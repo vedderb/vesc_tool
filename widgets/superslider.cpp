20f0e57853ba357565be50394f1474cb112d524d
Benjamin Vedder <benjamin@vedder.se>
2024-04-13 21:11:59 +0200

    Minor cleanup

--- widgets/superslider.cpp
+++ widgets/superslider.cpp
@@ -57,13 +57,13 @@ SuperSlider::SuperSlider(QWidget *parent)
     setStyleSheet("QSlider::handle { image: url(://res/images/slider1.png); }"
 
                   "QSlider::groove:horizontal {"
-                  "border: 1px solid #999999;"
+                  "border: 0px solid #999999;"
                   "height: 20px;"
-                  "background: qlineargradient(x1:0, y1:0, x2:0, y2:1, stop:0 #B1B1B1, stop:1 #c4c4c4);"
+                  "background: #b4b4b4;"
                   "margin: 2px 0;}"
 
                   "QSlider::handle:horizontal {"
-                  "background: qlineargradient(x1:0, y1:0, x2:1, y2:1, stop:0 #b4b4b4, stop:1 #8f8f8f);"
+                  "background: #b4b4b4;"
                   "border: 0px solid #5c5c5c;"
                   "width: 14px;"
                   "margin: 0px 0;"

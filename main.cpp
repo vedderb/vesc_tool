2d3da9d2b242e446a44fa5bf3b098703128a9790
Benjamin Vedder <benjamin@vedder.se>
2024-08-15 09:47:55 +0200

    Do not create QApplication when only downloading package archive

--- main.cpp
+++ main.cpp
@@ -1404,6 +1404,8 @@ int main(int argc, char *argv[])
             qCritical() << "Could not start TcpHub on port" << tcpPort;
             qApp->quit();
         }
+    } else if (downloadPackageArchive) {
+        return 0;
     } else {
         QApplication *a = new QApplication(argc, argv);
         app = a;
@@ -1550,8 +1552,6 @@ int main(int argc, char *argv[])
         } else if (useBoardSetupWindow){
             bw = new BoardSetupWindow;
             bw->show();
-        } else if (downloadPackageArchive) {
-            return 0;
         } else {
             QPixmapCache::setCacheLimit(256000);
             w = new MainWindow;

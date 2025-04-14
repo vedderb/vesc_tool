836b75181ab7c8ed5baf89361b38b06edd6e188a
Benjamin Vedder <benjamin@vedder.se>
2025-04-14 16:29:58 +0200

    Updated encoder example

--- res/Lisp/Examples/encoder_sincos_cal.lisp
+++ res/Lisp/Examples/encoder_sincos_cal.lisp
@@ -5,8 +5,8 @@
 ; 2. The amplitudes and offsets are now in s-amp, s-ofs, c-amp and c-ofs
 ; 3. Go to FOC->Encoder and enter the calculated amplitudes and offsets
 
-(def raw-sin (get-adc 0))
-(def raw-cos (get-adc 1))
+(def raw-sin (get-adc 20))
+(def raw-cos (get-adc 21))
 
 (def out-sin 0.0)
 (def out-cos 0.0)
@@ -32,7 +32,7 @@
         (setq c-ofs (/ (+ c-max c-min) 2.0))
         (setq s-amp (/ (- s-max s-min) 2.0))
         (setq c-amp (/ (- c-max c-min) 2.0))
-        
+
         (if print-res {
                 (print (str-merge (str-from-n s-amp "Sin Amp: %.3f ") (str-from-n s-ofs "Sin Offset: %.3f ")))
                 (print (str-merge (str-from-n c-amp "Cos Amp: %.3f ") (str-from-n c-ofs "Cos Offset: %.3f ")))
@@ -44,22 +44,37 @@
     (- val (* filter-const (- val sample)))
 )
 
-(loopwhile t {
-        (def raw-sin (lpf raw-sin (get-adc 0)))
-        (def raw-cos (lpf raw-cos (get-adc 1)))
-        
+(loopwhile-thd 100 t {
+        (def raw-sin (lpf raw-sin (get-adc 20)))
+        (def raw-cos (lpf raw-cos (get-adc 21)))
+
         (if (> raw-sin s-max) (def s-max raw-sin))
         (if (< raw-sin s-min) (def s-min raw-sin))
         (if (> raw-cos c-max) (def c-max raw-cos))
         (if (< raw-cos c-min) (def c-min raw-cos))
-        
+
         (def out-sin (/ (- raw-sin s-ofs) s-amp))
         (def out-cos (/ (- raw-cos c-ofs) c-amp))
         (def out-mod (sqrt (+ (* out-sin out-sin) (* out-cos out-cos))))
-        
+
         (if (and (> (- s-max s-min) 0.1) (> (- c-max c-min) 0.1))
             (calc-amp-ofs false)
         )
-        
+
         (sleep 0.01)
 })
+
+(defun detect-apply-config (current) {
+        (foc-openloop current 60)
+        (sleep 0.1)
+        (reset-max-min)
+
+        (looprange i 0 60 {
+                (foc-openloop current 60)
+                (sleep 0.1)
+        })
+
+        (conf-enc-sincos s-amp c-amp s-ofs c-ofs)
+        (conf-store)
+})
+
